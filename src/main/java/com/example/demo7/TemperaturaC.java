ad28bdbb069daf2b6860a62b6ca492c14092370b
Angel <cell9125@gmail.com>
2023-07-25 14:14:26 -0500

    Temperature Corrected/ Display update

--- src/main/java/com/example/demo7/TemperaturaC.java
+++ src/main/java/com/example/demo7/TemperaturaC.java
@@ -8,19 +8,26 @@ public class TemperaturaC {
         if(valDe == "Celcius"){
             if(valA == "Kelvin"){
                 resp = Integer.parseInt(valInput) + 273.15;
+            }else if(valA == "Farenheit"){
+                resp = (double)(Integer.parseInt(valInput) * 9/5) + 32;
             }else{
-                resp = (double)(Integer.parseInt(valDe) * 9/5) + 32;
+                resp = (double) Integer.parseInt(valInput);
             }
         }else if(valDe == "Kelvin"){
             if(valA == "Celcius"){
                 resp = Integer.parseInt(valInput) - 273.15;
-            }else{
+            }else if(valA == "Farenheit"){
                 resp = (Integer.parseInt(valInput) - 273.15)*9/5 + 32;
+            }else{
+                resp = (double) Integer.parseInt(valInput);
             }
         }else{
             if(valA == "Kelvin"){
                 resp = (Integer.parseInt(valInput) - 32) * 5/9 + 273.15;
-            }else{
+            }else if(valA == "Farenheit"){
+                resp = (double) Integer.parseInt(valInput);
+            }
+            else {
                 resp = (double)((Integer.parseInt(valInput) - 32)*5/9);
             }
         }
